e62885da1235e615cb72e6681c35ac9291f51c38
shariatikumail <shariati.kumail13@gmail.com>
2024-08-19 22:11:09 +0300

    learned new things

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Kumail Shariati 🚀</h1>
+      <h1 className="text-9xl font-bold text-blue-500">Kumail Shariati 🚀</h1>
+      <div className="bg-yellow-400 w-[500px] h-[250px] flex items-center justify-center rounded-xl">
+        <h1 className="text-4xl font-bold text-white">Student</h1>
+        </div>
     </main>
   );
 }
