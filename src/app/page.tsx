9bb09f7e4cdfff037e2265ab443c16b9cfd03053
shariatikumail <shariati.kumail13@gmail.com>
2024-08-21 14:57:48 +0300

    add shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-9xl font-bold bg-gradient-to-r from-blue-500 to-pink-700 text-transparent bg-clip-text">Kumail Shariati </h1>
-      <div className="bg-yellow-400 w-[500px] h-[250px] flex flex-col items-center justify-center rounded-xl shadow-xl">
-        <p className="text-5xl font-bold text-white">Student</p>
-        <p className="text-4xl font-bold text-white">Student</p>
-        <p className="text-3xl font-bold text-white">Student</p>
-        <p className="text-2xl font-bold text-white">Student</p>
-        <p className="text-xl font-bold text-white">Student</p>
-        <p className="text-lg font-bold text-white">Student</p>
+    <body className="bg-black">
+      
+    <div className="bg-black p-5 grid grid-cols-6  flex-2 text-left">
+       <a href="" className="text-white text-left ">Dashboard</a>
+      </div>
+      <main className="flex min-h-screen flex-col items-center justify-between bg-black p-24 h[200]">
+      <h1 className="text-9xl bg-gradient-to-r from-pink-950 to-pink-700 text-transparent bg-clip-text m-11 ">testing</h1>
+        <div className="text-white flex-col grid-cols-1">want to buy it for upload you codes?
+           <button>click</button>
+           <Button>Click Me</Button>
         </div>
-    </main>
+      </main>
+    </body>
   );
 }
